3846edd177edc217d533542349a654e28e75bcea
Santosh177 <117721403+Santosh177@users.noreply.github.com>
2023-02-17 14:54:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name"> Todo List</h1>
+    <p data-ns-test="project-description">we have to render which is written in input field</p>
+    
+    </div>
   )
 }
 
